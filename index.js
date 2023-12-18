e77f88527a2ec293c198b819c954149d6d3ec4a4
SachinSangaal <129685156+SachinSangaal@users.noreply.github.com>
2023-12-18 18:45:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,94 +1,155 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const { spawn } = require('child_process');
-const twilio = require('twilio');
-const translate = require('node-google-translate-skidz');
-const app = express();
-const port = 4000;
+// Copyright 2010-2012 Mikeal Rogers
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 
-const { exec } = require('child_process');
-app.use(express.static('public'));
-app.use(bodyParser.text()); // Parse request body as plain text
+'use strict'
 
-app.set('view engine', 'ejs');
+var extend = require('extend')
+var cookies = require('./lib/cookies')
+var helpers = require('./lib/helpers')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'sih',
-});
+var paramsHaveRequestBody = helpers.paramsHaveRequestBody
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
+// organize params for patch, post, put, head, del
+function initParams (uri, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
   }
-  console.log('Connected to MySQL database');
-});
-app.get('/',(req,res)=> {
-  res.sendFile(__dirname + '/home1.html');
+
+  var params = {}
+  if (options !== null && typeof options === 'object') {
+    extend(params, options, {uri: uri})
+  } else if (typeof uri === 'string') {
+    extend(params, {uri: uri})
+  } else {
+    extend(params, uri)
+  }
+
+  params.callback = callback || params.callback
+  return params
+}
+
+function request (uri, options, callback) {
+  if (typeof uri === 'undefined') {
+    throw new Error('undefined is not a valid uri or options object.')
+  }
+
+  var params = initParams(uri, options, callback)
+
+  if (params.method === 'HEAD' && paramsHaveRequestBody(params)) {
+    throw new Error('HTTP HEAD requests MUST NOT include a request body.')
+  }
+
+  return new request.Request(params)
+}
+
+function verbFunc (verb) {
+  var method = verb.toUpperCase()
+  return function (uri, options, callback) {
+    var params = initParams(uri, options, callback)
+    params.method = method
+    return request(params, params.callback)
+  }
+}
+
+// define like this to please codeintel/intellisense IDEs
+request.get = verbFunc('get')
+request.head = verbFunc('head')
+request.options = verbFunc('options')
+request.post = verbFunc('post')
+request.put = verbFunc('put')
+request.patch = verbFunc('patch')
+request.del = verbFunc('delete')
+request['delete'] = verbFunc('delete')
+
+request.jar = function (store) {
+  return cookies.jar(store)
+}
+
+request.cookie = function (str) {
+  return cookies.parse(str)
+}
+
+function wrapRequestMethod (method, options, requester, verb) {
+  return function (uri, opts, callback) {
+    var params = initParams(uri, opts, callback)
+
+    var target = {}
+    extend(true, target, options, params)
+
+    target.pool = params.pool || options.pool
+
+    if (verb) {
+      target.method = verb.toUpperCase()
+    }
+
+    if (typeof requester === 'function') {
+      method = requester
+    }
+
+    return method(target, target.callback)
+  }
+}
+
+request.defaults = function (options, requester) {
+  var self = this
+
+  options = options || {}
+
+  if (typeof options === 'function') {
+    requester = options
+    options = {}
+  }
+
+  var defaults = wrapRequestMethod(self, options, requester)
+
+  var verbs = ['get', 'head', 'post', 'put', 'patch', 'del', 'delete']
+  verbs.forEach(function (verb) {
+    defaults[verb] = wrapRequestMethod(self[verb], options, requester, verb)
+  })
+
+  defaults.cookie = wrapRequestMethod(self.cookie, options, requester)
+  defaults.jar = self.jar
+  defaults.defaults = self.defaults
+  return defaults
+}
+
+request.forever = function (agentOptions, optionsArg) {
+  var options = {}
+  if (optionsArg) {
+    extend(options, optionsArg)
+  }
+  if (agentOptions) {
+    options.agentOptions = agentOptions
+  }
+
+  options.forever = true
+  return request.defaults(options)
+}
+
+// Exports
+
+module.exports = request
+request.Request = require('./request')
+request.initParams = initParams
+
+// Backwards compatibility for request.debug
+Object.defineProperty(request, 'debug', {
+  enumerable: true,
+  get: function () {
+    return request.Request.debug
+  },
+  set: function (debug) {
+    request.Request.debug = debug
+  }
 })
-app.post('/save-spoken-text', (req, res) => {
-  const spokenText = req.body.text;
-
-  // Translate the spoken text from Telugu to English
-  translate({
-    text: spokenText,
-    source: 'te', // Source language (Telugu)
-    target: 'en', // Target language (English)
-  }, (result) => {
-    if (result.translation) {
-      // Display the translated text in the terminal
-      console.log('Received spoken text:', spokenText);
-      console.log('Translated text:', result.translation);
-
-      // Pass the translated text to a Python script using a child process
-      const pythonProcess = spawn('python', ['test.py', result.translation]);
-
-      // Handle data from the Python script if needed
-      pythonProcess.stdout.on('data', (data) => {
-        // Handle data from the Python script (if any)
-        console.log('Python script output:', data.toString());
-      });
-
-      // Handle errors or process exit
-      pythonProcess.on('error', (error) => {
-        console.error('Error running Python script:', error);
-        res.status(500).json({ error: 'An error occurred while running the Python script' });
-      });
-
-      pythonProcess.on('close', (code) => {
-        if (code !== 0) {
-          console.error(`Python script exited with code ${code}`);
-          res.status(500).json({ error: 'Python script exited with an error' });
-        } else {
-          res.sendStatus(200); // Send a success response
-        }
-      });
-    } else {
-      console.error('Translation Error:', result.error);
-      res.status(500).json({ error: 'Translation error' });
-    }
-  });
-});
-app.get('/fetchdata', (req, res) => {
-  const query = 'SELECT * FROM sample';
-  console.log("hello")
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Error executing MySQL query:', err);
-      res.status(500).send('Error fetching data');
-      return;
-    }
-
-    res.render('one.ejs', { data: results });
-  });
-});
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
